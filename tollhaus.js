cac6870e04fc6c32ca6385351b71a75737770dd0
KaRa901 <117648802+KaRa901@users.noreply.github.com>
2024-05-02 14:36:56 +0200

    Add files via upload

---
+++ tollhaus.js
@@ -0,0 +1,12 @@
+var video = document.getElementById("Media/Hendrik_Short_Exp_1_noblack.mp4");
+var btn = document.getElementById("myBtn");
+
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
